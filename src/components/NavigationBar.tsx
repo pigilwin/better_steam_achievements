f4cac2d44cf79c24ed3e2e09e19eb20b0b5341bb
Tim Remnant <timremnant@icloud.com>
2024-01-09 23:56:58 +0000

    fix: larger title

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -13,7 +13,7 @@ export const NavigationBar = ({}: NavigationBarProps): JSX.Element => {
     return (
         <nav className="flex justify-between items-center p-2 mx-4 bg-white rounded-md sticky top-2">
             <div className="w-full">
-                <span className="text-lg">Better Steam Achivements</span>
+                <span className="text-4xl">Better Steam Achivements</span>
             </div>
             <div className="w-full sm:w-auto self-end sm:self-center sm:flex flex-col sm:flex-row items-center justify-between h-full py-1 pb-4 sm:py-0 sm:pb-0">
                 <span className="p-2">
