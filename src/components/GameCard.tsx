523c2cc76bb580ed87f4afe1dc8567c20a5956e4
Tim Remnant <timremnant@icloud.com>
2024-01-19 12:11:41 +0000

    fix: ensure alt attribute is set

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -5,11 +5,12 @@ interface GameCardProps {
 }
 export const GameCard = ({game}: GameCardProps): JSX.Element => {
     const imageUrl = `https://cdn.akamai.steamstatic.com/steam/apps/${game.id}/library_hero.jpg`;
+    const altTitle = `Failed to load image for ${game.name}`
     return (
         <div className="relative flex flex-col items-center cursor-pointer">
-            <img src={imageUrl} className="object-cover pointer-events-none rounded-md"/>
+            <img src={imageUrl} className="object-cover pointer-events-none rounded-md" alt={altTitle}/>
             <h2 className="absolute top-2 mix-blend-color-burn text-xl">{game.name}</h2>
-            <p className="absolute bottom-2 mix-blend-color-burn text-xl">This game has {game.achievements.length} achievements</p>
+            <p className="absolute bottom-2 mix-blend-color-burn text-xl">This game has {Object.keys(game.achievements).length} achievements</p>
         </div>
     );
 };
