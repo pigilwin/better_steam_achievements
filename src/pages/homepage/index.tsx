a9a1b5bfe6db1a99da32edc63856b23a3e39a14a
Tim Remnant <timremnant@icloud.com>
2024-01-19 12:11:24 +0000

    feat: when the homepage is loaded, make sure the application loads the games if they are not loaded

--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -1,27 +1,36 @@
-import {ReactElement} from "react";
-import {useSelector} from "react-redux";
+import {ReactElement, useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
 
 import { GameCard } from "components/GameCard";
 import NoProfileSelected from 'pages/homepage/components/NoProfileSelected';
 
-import {Game, Games} from "store/types";
+import {Games} from "store/types";
 import {getSelectedProfileSelector} from "@store/application/applicationSlice";
-import {getGamesSelector, getIsLoadingSelector} from "@store/game/gameSlice";
+import {getGamesSelector, getHasGamesBeenLoadedSelector} from "@store/game/gameSlice";
+import {AppDispatch} from "@store/index";
+import {initialiseGames} from "@store/game/thunk";
 
 export const Index = (): ReactElement => {
 
+    const dispatch = useDispatch<AppDispatch>();
+
     const selectedProfile = useSelector(getSelectedProfileSelector);
-    const isLoading = useSelector(getIsLoadingSelector);
+    const hasGamesBeenLoaded = useSelector(getHasGamesBeenLoadedSelector);
+
+    useEffect(() => {
+        if (selectedProfile !== undefined) {
+            dispatch(initialiseGames(selectedProfile));
+        }
+    }, [hasGamesBeenLoaded, selectedProfile]);
+
     const games = useSelector(getGamesSelector);
 
     const children: ReactElement[] = [];
 
     if (selectedProfile === undefined) {
         children.push(<NoProfileSelected key="no-profile"/>);
-    } else if (isLoading) {
-        children.push(<p key="loading">Loading...</p>);
     } else {
-        children.push(<ProfileSelected gameCount={games.length} key="profile-selected"/>);
+        children.push(<ProfileSelected gameCount={Object.keys(games).length} key="profile-selected"/>);
         children.push(<Grid key="achievement-grid" games={games}/>);
     }
 
@@ -38,8 +47,8 @@ interface ProfileSelectedProps {
 const ProfileSelected = ({gameCount}: ProfileSelectedProps): ReactElement => {
     return (
         <article className="flex flex-col items-center justify-center bg-white w-full p-2 rounded-md">
-            <h2 className="text-xl">{gameCount} games have been fully completed</h2>
-            <p className="text-sm">A total of 50 achievements have been acquired.</p>
+            <h2 className="text-xl">{gameCount} games have been loaded</h2>
+            <p className="text-sm">A total of 0 achievements have been acquired.</p>
         </article>
     );
 }
@@ -49,8 +58,12 @@ interface GridProps {
 }
 const Grid = ({games}: GridProps): ReactElement => {
 
-    const cards = games.map((game) => <GameCard game={game}/>);
-
+    const cards = [];
+    for (const [key, value] of Object.entries(games)) {
+        cards.push(
+            <GameCard game={value} key={key}/>
+        );
+    }
     return (
         <article className="bg-white w-full p-2 rounded-md grid grid-cols-3 gap-4">
             {cards}
