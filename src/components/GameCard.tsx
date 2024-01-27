2a31932f139f991f64741d4b133632a8f6b604a7
Tim Remnant <timremnant@icloud.com>
2024-01-27 18:25:15 +0000

    feat: game card can now be clicked

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,12 +3,13 @@ import {ReactElement} from "react";
 
 interface GameCardProps {
     game: Game;
+    onClickHandler: () => void;
 }
-export const GameCard = ({game}: GameCardProps): ReactElement => {
+export const GameCard = ({game, onClickHandler}: GameCardProps): ReactElement => {
     const imageUrl = `https://cdn.akamai.steamstatic.com/steam/apps/${game.id}/library_hero.jpg`;
     const altTitle = `Failed to load image for ${game.name}`;
     return (
-        <div className="relative flex flex-col items-center cursor-pointer">
+        <div className="relative flex flex-col items-center cursor-pointer" onClick={onClickHandler}>
             <img src={imageUrl} className="object-cover pointer-events-none rounded-md" alt={altTitle}/>
             <h2 className="absolute top-2 text-white text-xl">{game.name}</h2>
             <p className="absolute bottom-2 text-white text-xl">This game has {Object.keys(game.achievements).length} achievements</p>
--- src/pages/homepage/components/CompletedGamesGrid.tsx
+++ src/pages/homepage/components/CompletedGamesGrid.tsx
@@ -4,13 +4,19 @@ import {useSelector} from "react-redux";
 import {getCompletedGamesSelector} from "@store/game/gameSlice";
 import ProfileSelected from "./ProfileSelected";
 import {GameCard} from "@components/GameCard";
+import {useNavigate} from "react-router-dom";
 
 export default (): ReactElement => {
     const games = useSelector(getCompletedGamesSelector);
+    const navigate = useNavigate();
+
     const cards = [];
     for (const [key, value] of Object.entries(games)) {
+        const onGameSelectedHandler = () => {
+            navigate(`/games/${value.storedKey}`);
+        };
         cards.push(
-            <GameCard game={value} key={key}/>
+            <GameCard game={value} key={key} onClickHandler={onGameSelectedHandler}/>
         );
     }
 
