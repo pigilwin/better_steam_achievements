176aa3df4043ba1d6e7085d0a692914f8a7ea95b
Tim Remnant <timremnant@icloud.com>
2024-02-01 18:22:50 +0000

    feat: adding ToggleSwitch

--- src/components/Inputs.tsx
+++ src/components/Inputs.tsx
@@ -71,3 +71,25 @@ export const Range = ({label, onChange, id, value, min, max, column}: RangeProps
         </Wrapper>
     );
 };
+
+interface ToggleSwitchProps {
+    value: boolean;
+    title: string;
+    onChange: ChangeEventHandler<HTMLInputElement>;
+}
+export const ToggleSwitch = ({title, onChange, value}: ToggleSwitchProps): ReactElement => {
+    return (
+        <div className="flex flex-col">
+            <div className="ml-3 text-xl">{title}</div>
+            <div className="toggle-switch flex flex-col p-4">
+                <label className="flex items-center cursor-pointer">
+                    <div className="relative">
+                        <input onChange={onChange} checked={value} type="checkbox" className="hidden" />
+                        <div className="toggle__line w-10 h-4 bg-white rounded-full shadow-inner"></div>
+                        <div className="toggle__dot absolute w-6 h-6 bg-white rounded-full shadow inset-y-0 left-0"></div>
+                    </div>
+                </label>
+            </div>
+        </div>
+    );
+}
