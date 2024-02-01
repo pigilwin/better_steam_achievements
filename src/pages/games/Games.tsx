af402206e39939937cbc555bedb3206ae6687106
Tim Remnant <timremnant@icloud.com>
2024-02-01 20:57:01 +0000

    fix: ensure title is centralised

--- src/pages/games/Games.tsx
+++ src/pages/games/Games.tsx
@@ -4,8 +4,9 @@ import {useSelector} from "react-redux";
 import {getGames} from "@store/game/gameSlice";
 import {RootState} from "@store/rootReducer";
 import {GameCard} from "@components/GameCard";
-import {Games, PotentialProfile} from "@store/types";
 import {ResizableGrid} from "@components/ResizableGrid";
+import {PotentialProfile} from "@store/application/profile";
+import {Games} from "@store/game/game";
 
 interface GameProps {
     profile: PotentialProfile
@@ -34,7 +35,7 @@ export const Index = ({profile}: GameProps): ReactElement | null => {
 
     return (
         <section className="flex flex-col gap-2 m-4">
-            <article className="bg-white w-full p-2 rounded-md flex justify-end h-full gap-1">
+            <article className="bg-white w-full p-2 rounded-md flex justify-center h-full gap-1">
                 <p>All games</p>
             </article>
             <ResizableGrid elements={cards} rangeLabel="How many columns to show?" key="resizable-grid"/>
